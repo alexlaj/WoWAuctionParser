8185790a913b2aa69d7e277c53ba038e87279480
alexlaj <alexlajeunesse@gmail.com>
2015-02-09 22:56:00 -0800

    Changed logic
    Trying to pythonize the logic. Also use sql queries more while still
    keeping total number of queries down

--- ahParse.py
+++ ahParse.py
@@ -1,41 +1,26 @@
-# split order data into realms
-# split realm data into orders per user
-# for each realm
-    # get realm auction dump from blizzard
-    # find unique items in realm orders
-    # find all instances of unique items from realm orders in auction data
-    # for each user on realm
-        # find matching orders
-        # create and send email with items that match conditions
-
-def getOrders(conn):
-    c = conn.cursor(pymysql.cursors.DictCursor)
-    
-    c.execute("SELECT * FROM orders")
-
-    orders = c.fetchall()
-    olist = {}
-    found = 0
-    for i in orders:
-        if i['email'] in olist:
-            olist[i['email']].append(i['item'])
-        else:
-            olist[i['email']] = [i['item']]
-          
-    c.close()
-    return olist
-
-def getUniqueItems(orders):
-    uItems = []
-    for x in orders:
-        uItems += orders[x]
-    return (set(items))
-
-def getAuctionData():
-    HOST = "http://us.battle.net/api/wow/auction/data/"
-    REALM = "Lightbringer"
-
-    url = HOST + REALM
+import pymysql
+import requests
+# For printing output with special characters in it
+import sys
+import codecs
+if sys.stdout.encoding != 'cp850':
+  sys.stdout = codecs.getwriter('cp850')(sys.stdout.buffer, 'strict')
+if sys.stderr.encoding != 'cp850':
+  sys.stderr = codecs.getwriter('cp850')(sys.stderr.buffer, 'strict')
+  
+# Extract all orders for locale from database
+def getOrders(c, locale):
+    c.execute("SELECT * FROM realms WHERE locale='"+locale+"'")
+    data = c.fetchall()
+    orders = []
+    for i in data:
+        c.execute("SELECT email, locale, itemID, operator, price FROM orders WHERE realm='"+i['realm']+"' AND locale='na'")
+        d = c.fetchall()
+        orders.append([i['realm'],d])
+    return orders
+# Get auction data from blizzard for realm in locale
+def getAuctionData(locale, realm):
+    url = "http://" + locale + ".battle.net/api/wow/auction/data/" + realm
     resp = requests.get(url)
     data = resp.json()
     url = data['files'][0]['url']
@@ -43,3 +28,41 @@ def getAuctionData():
     resp = requests.get(url)
     data = resp.json()
     return data
+# Find all instances of itemIDs in the auction data
+def findItems(itemID, data):
+    fItems = [[] for x in range(len(itemID))] 
+    allAuctions = data['auctions']['auctions']
+    for j in range(len(itemID)):
+        for i in allAuctions:
+            if i['item'] == int(itemID[j]):
+                fItems[j].append(itemID[j])
+                # convert to gold per item
+                fItems[j].append([i['buyout']/(i['quantity']*10000),i['buyout']/10000,i['quantity']])
+    fItems = filter(None, fItems)            
+    for i in fItems:
+        print(i)
+        print()
+    cItems = []
+    for i in fItems:
+        cItems.append([fItems[i][0], sorted(fItems[i][1:], key=lambda x:x[0])])           
+    return cItems
+    
+# Main program
+connection = pymysql.connect(host='192.168.1.73', port=3306, user='pythonuser', passwd='pypyaccessingtehdb', db='auction_parser_dev')
+c = connection.cursor(pymysql.cursors.DictCursor)
+
+naOrders = getOrders(c, 'na')
+for i in naOrders:
+    aData = getAuctionData('us', i[0])
+    c.execute("SELECT DISTINCT itemID FROM orders WHERE realm='"+i[0]+"' AND locale='na'")
+    uItems = c.fetchall()
+    uItems2 = []
+    for j in uItems:
+        uItems2.append(j['itemID'])
+    fItems = findItems(uItems2, aData)
+
+    for i in fItems:
+        print(i)
+        print()
+    print(fItems)
+
--- db_learn.py
+++ db_learn.py
@@ -1,31 +1,45 @@
 import pymysql
+import requests
+import json
+import urllib.request
+import smtplib
+from email.mime.text import MIMEText
 
-connection = pymysql.connect(host='192.168.1.73', port=3306, user='pythonuser', passwd='pypyaccessingtehdb', db='pylearn')
+connection = pymysql.connect(host='192.168.1.73', port=3306, user='pythonuser', passwd='pypyaccessingtehdb', db='auction_parser_dev')
 
 #curs = connection.cursor()
 
 #curs.execute("CREATE TABLE items (id int not null primary key auto_increment, item int);")
 
+
+#   locale{ realm{ email{ orders: [] } } }
+
 c = connection.cursor(pymysql.cursors.DictCursor)
 
-c.execute("SELECT * FROM orders")
+data = [[32837, [-1]], [32838, [[-1]]], [118874, [[40000.0, 40000.0, 1]]], [118892, [[21000.0, 21000.0, 1], [34000.0, 34000.0, 1], [36500.0, 36500.0, 1], [36630.0, 36630.0, 1], [38025.0, 38025.0, 1], [39000.0, 39000.0, 1], [40000.0, 40000.0, 1]]]]
+msg = {}
+msg['Message'] = ''
+for i in data:
+    url = "http://www.wowdb.com/items/" + str(i[0])
+    req = urllib.request.urlopen(url)
+    finalurl = req.geturl()
+    finalurl = finalurl.replace("http://www.wowdb.com/items/", "")
+    finalurl = finalurl.replace(str(i[0])+"-", "")
+    finalurl = finalurl.replace("?cookieTest=1", "")
+    finalurl = finalurl.replace("-", " ")
+    msg['Message'] = msg['Message'] + finalurl + " " + str(i) + "\n"
 
-#print(c.description)
-orders = {}
-orders = c.fetchall()
-olist = {}
-found = 0
-for i in orders:
-        #print(i)
-        if i['email'] in olist:
-            olist[i['email']].append(i['item'])
-        else:
-            olist[i['email']] = [i['item']]
-uItems = []
-for x in olist:
-        uItems += olist[x]
-print(set(uItems))
-print(olist)    
+
+    
+msg['Subject'] = 'Items found!'
+msg['From'] = 'crens.lightbringer.ah.mailer@gmail.com'
+msg['To'] = 'alexlajwow@gmail.com'
+
+server = smtplib.SMTP("smtp.gmail.com", 587) #or port 465 doesn't seem to work!
+server.ehlo()
+server.starttls()
+server.login(msg['From'], '#finddatitemdawg')
+server.sendmail(msg['From'], msg['To'], msg['Message'])
 
 c.close()
 connection.commit()
